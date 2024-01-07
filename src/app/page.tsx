a92849c15bd2de8dbaff601883160d81345f528b
GitAnna90 <kzaar@free.fr>
2024-01-07 15:26:46 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>
+          <big> **** BIENVENUE A TOUS ****</big>
+          <p>
+            <big>  HELLO VOILA MON APPLICATION POUR TESTER NextJS !!!!</big>
+          </p>
+      </p>
+    </main>
+  )
+}
